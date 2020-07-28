be29a1d8a4393d3a9e99d94179907bf4d37232d0
ductai26998 <ductai26998@gmail.com>
2020-07-28 22:49:09 +0700

    create feature to add avatar

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,14 @@
 var bcrypt = require('bcrypt');
 var db = require('../db');
+var cloudinary = require('cloudinary').v2;
 
 const shortid = require('shortid');
+
+cloudinary.config({ 
+  cloud_name: 'ductai26998', 
+  api_key: '744596793339385', 
+  api_secret: '_jqie405YJR-Jct1XSBwKkbUUy8' 
+});
 
 module.exports.get = (request, response) => {
   response.render('users/index', {
@@ -26,6 +33,14 @@ module.exports.create = (request, response) => {
 
 module.exports.postCreate = (request, response) => {
   request.body.id = shortid.generate();
+  if (!request.file) {
+    request.body.avatar = "updates/avt.jpg";
+  } else {
+    request.body.avatar = request.file.path.split("\\").slice(1).join("/");
+    cloudinary.uploader.upload(request.body.avatar, function(error, result) {
+      console.log(result, error)
+    });
+  }
   request.body.isAdmin = "false";
   request.body.wrongLoginCount = 0;
   bcrypt.hash(request.body.password, 10, function(err, hash) {
@@ -45,15 +60,52 @@ module.exports.delete = (request, response) => {
   response.redirect('/users');
 };
 
-module.exports.update = (request, response) => {
-  response.render('users/update', {id: request.params.id});
+module.exports.profile = (request, response) => {
+  response.render('users/profile', {
+    id: request.params.id,
+    user: db.get('users').find({id: request.params.id}).value(),
+    getAvatarUrl: function(url) {
+      if (url.indexOf('http') === 0) {
+        return url;
+      }
+      return '/' + url;
+    }
+  });
 };
 
-module.exports.postUpdate = (request, response) => {
+module.exports.postProfile = (request, response) => {
   var userId = request.params.id;
   db.get('users')
     .find({id: userId})
-    .assign({name: request.body.name})
+    .assign({
+      name: request.body.name,
+      phone: request.body.phone,
+      email: request.body.email
+    })
     .write();
   response.redirect('/users');
+};
+
+module.exports.avatar = (request, response) => {
+  response.render('users/avatar', {
+    id: request.params.id
+  });
+}
+
+module.exports.postAvatar = async (request, response) => {
+  var id = request.params.id;
+  if (!request.file) {
+    request.body.avatar = "https://hinhanhdephd.com/wp-content/uploads/2020/07/hinh-anh-mat-cuoi-sieu-cute-dang-yeu-14-600x375.jpg";
+  } else {
+    request.body.avatar = request.file.path.split("\\").slice(1).join("/");
+    const result = await cloudinary.uploader.upload(request.file.path);
+
+    db.get('users')
+    .find({ id: id })
+    .assign({
+      avatar: request.body.avatar
+    })
+    .write();
+  }
+  response.redirect('/users/' + id + '/profile');
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var multer = require('multer');
+
 var controller = require('../controllers/user.controller');
 var validate = require('../validates/user.validate');
 var authMiddleware = require('../middlewares/auth.middleware');
 
 var router = express.Router();
+var upload = multer({dest: './public/updates/'});
 
 // https://expressjs.com/en/starter/basic-routing.html
 router.get("/", controller.get);
@@ -12,12 +15,21 @@ router.get('/search', controller.search);
 
 router.get('/create', controller.create);
 
-router.post('/create', validate.postCreate, controller.postCreate);
+router.post('/create', 
+	upload.single('avatar'), 
+	validate.postCreate, 
+	controller.postCreate);
 
 router.get("/:id/delete", controller.delete);
 
-router.get('/:id/update', controller.update);
+router.get("/:id/profile", controller.profile);
 
-router.post('/:id/update', controller.postUpdate);
+router.post('/:id/profile', controller.postProfile);
+
+router.get("/:id/profile/avatar", controller.avatar);
+
+router.post("/:id/profile/avatar", 
+	upload.single('avatar'), 
+	controller.postAvatar);
 
 module.exports = router;
